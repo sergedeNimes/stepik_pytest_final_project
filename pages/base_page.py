021c4723ec3297357587795fede8eafb6c78ced7
me <me@only.com>
2020-07-30 20:56:38 +0300

    в main_page.py создал класс MainPage с методом go_to_login_page; в test_main_page.py корректировка test_guest_can_go_to_login_page.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,7 @@
 class BasePage():
-	def __init__(self, browser, url):
-		self.browser = browser
-		self.url = url
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
 
-	def open(self):
-		self.browser.get(self.url)
-
-	'''
-	def test_add_to_cart(browser):
-	    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-	    page.open()                           # открываем страницу в браузере
-	    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-	    page.add_product_to_cart()            # жмем кнопку добавить в корзину 
-	    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
-    '''
+    def open(self):
+        self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,18 @@
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()   
+
+
+'''
+def test_add_to_cart(browser):
+    page = ProductPage(url="", browser)   # инициализируем объект Page Object
+    page.open()                           # открываем страницу в браузере
+    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+    page.add_product_to_cart()            # жмем кнопку добавить в корзину 
+    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+'''    
