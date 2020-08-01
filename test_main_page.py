8079950d04cdc39bd88e020ee54f950d96ca7433
me <me@only.com>
2020-08-01 14:02:58 +0300

    в классе MainPage создал метод should_be_login_link; добавил test_main_page:: test_guest_should_see_login_link; в классе BasePage в __init__ добавил implicitly_wait, добавил метод is_element_present.

--- test_main_page.py
+++ test_main_page.py
@@ -7,12 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()   
 
-
-'''
-def test_add_to_cart(browser):
-    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-    page.open()                           # открываем страницу в браузере
-    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-    page.add_product_to_cart()            # жмем кнопку добавить в корзину 
-    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
-'''    
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
