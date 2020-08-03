1abb1fb621a38da67c8b9c47cca87725dfb0d2fd
me <me@only.com>
2020-08-03 22:26:22 +0300

    изменение conftest.py, login_page.py

--- conftest.py
+++ conftest.py
@@ -8,16 +8,25 @@ def pytest_addoption(parser):
                      action='store',
                      default='en',
                      help='choose language: --language=es|fr|ru')
+    parser.addoption('--browser',
+                     action='store',
+                     default='chrome',
+                     help='choose browser: --browser=chrome|firefox')
 
 
 @pytest.fixture(scope='function')
 def browser(request):
-    user_language = request.config.getoption('language')
+    _language = request.config.getoption('language')
+    _browser = request.config.getoption('browser')
     
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    if _browser == 'chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': _language})
+        browser = webdriver.Chrome(options=options)
+    elif _browser == 'firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', _language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     
-    browser = webdriver.Chrome(options=options)
     yield browser
-    
     browser.quit()
--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url.endswith("/login/"), "Login url is not correct"
+        assert "/login/" in self.browser.current_url, "Login url is not correct"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
