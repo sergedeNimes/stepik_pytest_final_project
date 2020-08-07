74a6d777e50d91b32923bec0fd0c0778d2ff7395
me <me@only.com>
2020-08-07 15:39:27 +0300

    в файле test_main_page.py объединил два теста в класс TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -6,13 +6,19 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):    
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_openend_from_main_page(browser):
@@ -22,10 +28,3 @@ def test_guest_cant_see_product_in_basket_openend_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_empty_basket_message()
-
-
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
