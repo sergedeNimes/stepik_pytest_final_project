dfe9584a401ee807a10bd654fa1820ffb113a505
me <me@only.com>
2020-08-08 18:31:02 +0300

    в LoginPage добавил метод register_new_user; в LoginPageLocators добавил селекторы для регистрации юзера

--- pages/locators.py
+++ pages/locators.py
@@ -15,6 +15,10 @@ class BasketPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, ".register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, ".register_form button")
 
 
 class ProductPageLocators():
