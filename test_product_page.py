aa5dbe9772091bfe8276be5822b2ebf1d765b782
me <me@only.com>
2020-08-08 18:06:08 +0300

    в файл test_product_page.py добавил класс для тестов с двумя тестами

--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,23 @@ book1 = "/coders-at-work_207"
 book2 = "/the-city-and-the-stars_95"
 
 
+class TestUserAddToBasketFromProductPage():
+    def test_user_cant_see_success_message(self, browser):
+        link = f"{catalog}{book1}"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser, offer):
+        link = f"{catalog}{book1}/?promo=offer{offer}"
+        page = ProductPage(browser, link)
+        page.open()
+        print(f"\n{link}")
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_product_in_basket()
+
+
 # @pytest.mark.parametrize('offer', range(10))
 @pytest.mark.parametrize('offer', [0, 1, 2, 3, 4, 5, 6, 
                                    pytest.param(7, marks=pytest.mark.xfail(
