1d33dcd91b2f6057724fcb8f7c60618dd1777194
me <me@only.com>
2020-08-08 18:56:59 +0300

    в BasePage добавил проверку should_be_authorized_user и соответствующий селектор в BasePageLocators

--- pages/base_page.py
+++ pages/base_page.py
@@ -52,6 +52,11 @@ class BasePage():
         return False
 
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), \
+                                       "User icon is not presented," \
+                                       " probably unauthorised user"
+
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
                                        "Login link is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
