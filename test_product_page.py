f42c942b55358f1506262f51e72575e04e683c0c
me <me@only.com>
2020-08-08 19:20:02 +0300

    в тестовый класс в test_product_page.py добавил фикстуру setup для регистрации нового пользователя

--- test_product_page.py
+++ test_product_page.py
@@ -1,44 +1,51 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-import pytest
-from time import sleep
+import pytest, time
 
 
 catalog = "http://selenium1py.pythonanywhere.com/catalogue"
 book1 = "/coders-at-work_207"
 book2 = "/the-city-and-the-stars_95"
 
-
+@pytest.mark.need_review
 class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        user = str(time.time()) + "@fakemail.org", "preved_ʕ•ᴥ•ʔ_medved"
+        # user = str(time.time()) + "@fakemail.org", "passworD:))"
+        page = ProductPage(browser, catalog)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+        login_page.register_new_user(*user)
+        login_page.should_be_authorized_user()
+
     def test_user_cant_see_success_message(self, browser):
         link = f"{catalog}{book1}"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
-    def test_user_can_add_product_to_basket(self, browser, offer):
-        link = f"{catalog}{book1}/?promo=offer{offer}"
+    def test_user_can_add_product_to_basket(self, browser):
+        link = f"{catalog}{book1}"
         page = ProductPage(browser, link)
         page.open()
-        print(f"\n{link}")
         page.add_product_to_basket()
-        page.solve_quiz_and_get_code()
         page.should_be_product_in_basket()
 
 
-# @pytest.mark.parametrize('offer', range(10))
+@pytest.mark.skip
 @pytest.mark.parametrize('offer', [0, 1, 2, 3, 4, 5, 6, 
                                    pytest.param(7, marks=pytest.mark.xfail(
                                                    reason="BUG: product name in success message not correct")),
                                    8, 9],
                         )
-@pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser, offer):
     link = f"{catalog}{book1}/?promo=offer{offer}"
     page = ProductPage(browser, link)
     page.open()
-    print(f"\n{link}")
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_product_in_basket()
